d1b2e6cb855ea0bdf372ea2b551094ecad791674
Villa01 <javillatoro1@gmail.com>
2022-06-06 06:53:40 -0600

    Added access to read

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, Task
+    can :manage, Task, owner_id: user.id
+    can :read, Task, participating_users: { user_id: user.id}
   end
 end
